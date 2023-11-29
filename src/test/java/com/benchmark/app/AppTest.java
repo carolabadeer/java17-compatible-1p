d349702bea12f63112e13026831be7d36994615d
Carol Abadeer <abadcaro@amazon.com>
2023-11-28 16:05:19 -0800

    Add unit test for java 17 compatible 1p dependency

--- src/test/java/com/benchmark/app/AppTest.java
+++ src/test/java/com/benchmark/app/AppTest.java
@@ -5,13 +5,13 @@ import org.junit.Assert;
 
 public class AppTest 
 {
-    // @Test 
-    // public void test1pDependency() {
-    //     /*
-    //      * This unit test verifies 1p dependency functionality
-    //      */
+    @Test 
+    public void test1pDependency() {
+        /*
+         * This unit test verifies 1p dependency functionality
+         */
 
-    //     String stringValue = App.callJava17Compatible1pDependency("Test Java 17 compatible 1p dependency");
-    //     Assert.assertEquals(stringValue, "Test Java 17 compatible 1p dependency");
-    // }
+        String stringValue = App.callJava17Compatible1pDependency("Test Java 17 compatible 1p dependency");
+        Assert.assertEquals(stringValue, "Test Java 17 compatible 1p dependency");
+    }
 }
